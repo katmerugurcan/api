044a4e59b56fd2bd9d7f2288f7fbedce0c3d2f41
katmerugurcan <katmerugurcan@gmail.com>
2022-08-29 12:25:49 +0300

    initialCommit

--- index.js
+++ index.js
@@ -1,23 +1,21 @@
 const express = require('express');
-const setData = require('./Tools/methods');
+const setData = require("./Tools/methods");
 const baseUrl = require('./Tools/svConfig');
 
-const PORT = process.env.PORT || 5000;
-
 const server = express();
-server.use(express.json());
-server.use(express.static("public"));
 
-server.get('/', async (req, res) => {
-    const users = await setData.Get(`${baseUrl}/users`);
-    res.send(users);
-});
+server.use(express.json({ extended: false }));
 
 server.get('/:path', async (req, res) => {
     const { path } = req.params;
     const data = await setData.Get(`${baseUrl}/${path}`);
     console.log("Data:1212==>> ", data);
-    res.send(data);
+    // res.send(data);
+    res.json({
+        status: 200,
+        message: "Success",
+        data: data
+    });
 });
 
 server.get("/:path/:id", async (req, res) => {
@@ -27,6 +25,7 @@ server.get("/:path/:id", async (req, res) => {
     res.status(200).json(data);
 });
 
+const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
     console.log(`http://localhost:${PORT} adresinden gelen istekler dinleniyor...`);
 });
