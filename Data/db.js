cb7cae605d7cc1aadcc2f8091e2e9fd90d5b7cb3
katmerugurcan <katmerugurcan@gmail.com>
2022-09-13 11:51:12 +0300

    commit

---
+++ Data/db.js
@@ -0,0 +1,28 @@
+const data = {
+    "users": [
+        {
+            "id": 0,
+            "userName": "Uğurcan Katmer",
+            "age": 24,
+            "birht": "08.08.1998"
+        },
+        {
+            "id": 1,
+            "userName": "Emir",
+            "age": 22,
+            "birht": "09.10.2000"
+        }
+    ],
+    "tasks": [
+        {
+            "id": 0,
+            "task": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries"
+        },
+        {
+            "id": 1,
+            "task": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries"
+        }
+    ]
+}
+
+module.exports = { data }; 
